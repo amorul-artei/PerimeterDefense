de2032e8b1de9816a0f31c7f4701b99bf26fc052
Voicu Anton Albu <97848459+amorul-artei@users.noreply.github.com>
2022-02-03 18:41:11 +0200

    Added cleanup phase and a parameter option for performance counters

--- processVideos.py
+++ processVideos.py
@@ -11,8 +11,7 @@ import ffmpeg
 
 import os
 import time
-#from os import listdir
-#from os.path import isfile, join
+import argparse
 
 
 def GetFormattedFileSize( size ):
@@ -40,38 +39,77 @@ def AnalyzeTimeline( videosList ):
         expectedCreationTime = f[ 1 ] + duration
 
 
-onlyVideos = [ f for f in os.listdir() if os.path.isfile( f ) and f.lower().endswith( ".mp4" ) ]
+def CleanupPreviousRun():
+    filesToRemove = [ f for f in os.listdir() if os.path.isfile( f ) and \
+        f.lower().startswith( videoAnalyzeRateOfChange.kTempFilePrefix ) ]
+    for f in filesToRemove:
+        os.remove( f )
 
-alreadyAnalyzedVideos = [ f for f in onlyVideos if "_ROC_analyzed" in f ]
-tobeAnalyzedVideos = list( set( onlyVideos ) - set( alreadyAnalyzedVideos ) )
 
-origNames = []
-if len( alreadyAnalyzedVideos ) > 0:
-    print( "Found previous analysis, skipping" )
-    for f in alreadyAnalyzedVideos:
-        origName = f[ :f.find( "_ROC_analyzed" ) ]
-        print( origName )
-        origNames.append( origName )
+def DoGoProSpecificCleanup():
+    # Remove all *lrv and *thm files
+    filesToRemove = [ f for f in os.listdir() if os.path.isfile( f ) and \
+        ( f.lower().endswith( ".lrv" ) or f.lower().endswith( ".thm" ) ) ]
+    for f in filesToRemove:
+        os.remove( f )
 
-tobeAnalyzedVideos = list( set( tobeAnalyzedVideos ) - set( origNames ) )
-tobeAnalyzedVideos = [ (f, os.path.getmtime( f ), os.path.getsize( f ) ) for f in tobeAnalyzedVideos ]
-tobeAnalyzedVideos.sort( key = lambda x: x[ 1 ] )
+    # Rename all *.360 to *.360.mp4
+    filesToRename = [ f for f in os.listdir() if os.path.isfile( f ) and f.lower().endswith( ".360" ) ]
+    for f in filesToRename:
+        os.rename( f, f + ".mp4" )
 
-for f in tobeAnalyzedVideos:
-    print( GetFormattedFileStats( f ) )
 
-print( "A total of %i videos to analyze" % len( tobeAnalyzedVideos ) )
+def runProcessVideos( args = None ):
+    CleanupPreviousRun()
+    DoGoProSpecificCleanup()
 
-#AnalyzeTimeline( tobeAnalyzedVideos )
+    onlyVideos = [ f for f in os.listdir() if os.path.isfile( f ) and f.lower().endswith( ".mp4" ) ]
 
-for a in tobeAnalyzedVideos:
+    alreadyAnalyzedVideos = [ f for f in onlyVideos if "_ROC_analyzed" in f ]
+    tobeAnalyzedVideos = list( set( onlyVideos ) - set( alreadyAnalyzedVideos ) )
+
+    origNames = []
+    if len( alreadyAnalyzedVideos ) > 0:
+        print( "Found previous analysis, skipping" )
+        for f in alreadyAnalyzedVideos:
+            origName = f[ :f.find( "_ROC_analyzed" ) ]
+            print( origName )
+            origNames.append( origName )
+
+    tobeAnalyzedVideos = list( set( tobeAnalyzedVideos ) - set( origNames ) )
+    tobeAnalyzedVideos = [ (f, os.path.getmtime( f ), os.path.getsize( f ) ) for f in tobeAnalyzedVideos ]
+    tobeAnalyzedVideos.sort( key = lambda x: x[ 1 ] )
+
+    for f in tobeAnalyzedVideos:
+        print( GetFormattedFileStats( f ) )
+
+    print( "A total of %i videos to analyze" % len( tobeAnalyzedVideos ) )
+
+    #AnalyzeTimeline( tobeAnalyzedVideos )
+
+    count = 1
+    for a in tobeAnalyzedVideos:
+        print( "" )
+        print( "" )
+        print( "------------------------------------------------------" )
+        print( "-----------------------%i/%i--------------------------" % (count, len( tobeAnalyzedVideos )) )
+        print( "Running analysis for " + GetFormattedFileStats( a ) )
+        videoAnalyzeRateOfChange.runRateOfChangeAnalysis( a[ 0 ], args )
+        print( "" )
+        count += 1
+
+
     print( "" )
-    print( "" )
-    print( "------------------------------------------------------" )
-    print( "Running analysis for " + GetFormattedFileStats( a ) )
-    videoAnalyzeRateOfChange.runRateOfChangeAnalysis( a[ 0 ] )
-    print( "" )
+    print( "All done." )
 
 
-print( "" )
-print( "All done." )
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument( "--verboseRunningTime", \
+        help="enables display of running time performance split per phases of the algorithm", action="store_true" )
+
+    args = parser.parse_args()
+    runProcessVideos( args )
+
+
+#TODO-Pri1 voicua: ability to carbon copy all output to a log file as well
