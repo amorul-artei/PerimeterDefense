f1453e92ae0a18706bdfad373de8f7e7f954b4a9
Voicu Anton Albu <97848459+amorul-artei@users.noreply.github.com>
2022-02-06 15:32:58 +0200

    Helper routines should be placed here. Added a file logger.

---
+++ videoAnalysisHelpers.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+#
+# Author: Voicu Anton Albu
+# Email: voicualbu@gmail.com
+# Revision History:
+#      04.02.2022 voicua: Created "videoAnalysisHelpers.py" to contain common constructs.
+
+import os
+
+class Logger:
+    def __init__( self, logFileName = None ):
+        self.fileHandle = None
+        if not logFileName is None:
+            self.fileHandle = open( logFileName, 'a' )
+
+    def PrintMessage( self, msg = None ):
+        if msg is None:
+            msg = ""
+        print( msg )
+        if not self.fileHandle is None:
+            self.fileHandle.write( msg + os.linesep )
+
+    def Close( self ):
+        self.fileHandle.close()
